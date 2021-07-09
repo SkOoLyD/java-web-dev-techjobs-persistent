2c101ad22cf7558cd453263a7dfe8eda4643832c
SkOoLyD <79675305+SkOoLyD@users.noreply.github.com>
2021-07-08 19:51:41 -0500

    Added string field for Location with getters and setters

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,37 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
-public class Employer extends AbstractEntity {
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 
+@Entity
+public class Employer<Public> extends AbstractEntity {
+
+    @NotEmpty
+    @Size(max = 80,message = "Location length must be under 80.")
+    private String location;
+
+    @OneToMany
+    @JoinColumn
+    private final List<Job> jobs = new ArrayList<Job>();
+
+    public Employer() {}
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
