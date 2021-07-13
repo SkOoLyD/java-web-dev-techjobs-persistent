3069e464a65c7d89cd557c9af01461d919d89d4e
SkOoLyD <79675305+SkOoLyD@users.noreply.github.com>
2021-07-12 19:08:45 -0500

    Almost done with part moving into part 3

--- queries.sql
+++ queries.sql
@@ -1,8 +1,11 @@
 ## Part 1: Test it with SQL
+
 SELECT column_name,data_type from information_schema.columns
 where table_schema = 'techjobs' AND table_name = 'job';
 
 ## Part 2: Test it with SQL
+
+SELECT name FROM employer WHERE location = "St.Louis City"
 
 ## Part 3: Test it with SQL
 
